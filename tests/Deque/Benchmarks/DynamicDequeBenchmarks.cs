12e181d06d575b97f1014462e05d17baeb5944eb
Avishai Dernis <avid29@live.com>
2020-04-06 07:40:42 -0400

    Added DynamicDequeBenchmarks

---
+++ tests/Deque/Benchmarks/DynamicDequeBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoreCollections.Generic;
+
+namespace CollectionsTest.Deque.Benchmarks
+{
+    [TestClass]
+    public class DynamicDequeBenchmarks : DequeBenchmark
+    {
+        public DynamicDequeBenchmarks()
+        {
+            deque = new DynamicDeque<int>(512);
+            for (int i = 0; i < 100; i++)
+            {
+                deque.PushBack(i);
+            }
+        }
+
+        [TestMethod]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void PushBackDeque()
+        {
+            PushBack();
+        }
+
+        [TestMethod]
+        public void PushFrontDeque()
+        {
+            PushFront();
+        }
+    }
+}
