4abfc6318025197831cc0970d34464c8ecb21af9
Avishai Dernis <avid29@live.com>
2020-04-12 06:24:55 -0400

    Added 0 initial items test

--- Benchmarks/Deque/ConstantOldDequeBenchmarks.cs
+++ Benchmarks/Deque/ConstantOldDequeBenchmarks.cs
@@ -10,7 +10,7 @@ namespace Benchmarks.Deque
     [JsonExporterAttribute.Brief]
     public class ConstantOldDequeBenchmarks : DequeBenchmark
     {
-        [Params(1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000)]
+        [Params(0, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000)]
         public int Items;
 
         [Params(1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000)]
